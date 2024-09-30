6f33ff87ae0823734cb7fae190880c02b7512e9d
Clémentine <clementine.bertrand@lilo.org>
2024-09-30 12:15:29 +0200

    Page d'accueil

--- src/Controller/VoituresController.php
+++ src/Controller/VoituresController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Voitures;
+use App\Repository\VoituresRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/voitures')]
 class VoituresController extends AbstractController
 {
-    #[Route('/voitures', name: 'app_voitures')]
-    public function index(): Response
+    #[Route('/', name: 'app_voitures')]
+    public function index(VoituresRepository $repository): Response
     {
+        $voitures = $repository->findAll();
+
         return $this->render('voitures/index.html.twig', [
-            'controller_name' => 'VoituresController',
+            'voitures' => $voitures,
         ]);
     }
 }
