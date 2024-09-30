5fa792e7dc6a5a3b00f6883b72a1cc1bcf0199e0
Clémentine <clementine.bertrand@lilo.org>
2024-09-30 12:24:55 +0200

    Page détail de la voiture

--- src/Controller/VoituresController.php
+++ src/Controller/VoituresController.php
@@ -14,13 +14,21 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/voitures')]
 class VoituresController extends AbstractController
 {
-    #[Route('/', name: 'app_voitures')]
+    #[Route('/', name: 'voitures')]
     public function index(VoituresRepository $repository): Response
     {
         $voitures = $repository->findAll();
 
         return $this->render('voitures/index.html.twig', [
             'voitures' => $voitures,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'voitures_show', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function show(?Voitures $voiture): Response
+    {
+        return $this->render('voitures/show.html.twig', [
+            'voiture' => $voiture,
         ]);
     }
 }
